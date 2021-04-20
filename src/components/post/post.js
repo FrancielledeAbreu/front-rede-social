1eac895bd63549e9a2ed7f764b99ec6e88497359
Francielle de Abreu <francielledeabreus@gmail.com>
2021-04-19 22:47:35 -0300

    feat: posts created and timeline done

---
+++ src/components/post/post.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import Collapse from "@material-ui/core/Collapse";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+
+import { useLocation } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%",
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+const Post = ({
+  author,
+  title,
+  posted_on,
+  image,
+  description,
+  comment,
+  like,
+  likeAction,
+  commentAction,
+}) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const location = useLocation();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {author && author[0]}
+          </Avatar>
+        }
+        title={title}
+        subheader={`${author} em ${posted_on && posted_on.slice(0, 10)}`}
+      />
+
+      <CardMedia className={classes.media} image={image} title="Paella dish" />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {description}
+        </Typography>
+      </CardContent>
+      {location.pathname !== "/timeline" && (
+        <>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites" onClick={likeAction}>
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="add to favorites" onClick={commentAction}>
+              <ChatBubbleOutlineIcon />
+            </IconButton>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {like && like.length > 0 && (
+                <Typography paragraph>Likes : {like && like.length}</Typography>
+              )}
+              {comment &&
+                comment.map((comment, i) => {
+                  return (
+                    <>
+                      <Typography paragraph key={i}>
+                        {comment.author.username} : {comment.comment}
+                      </Typography>
+                      {comment.commented_on}
+                    </>
+                  );
+                })}
+            </Typography>
+          </Collapse>
+        </>
+      )}
+    </Card>
+  );
+};
+
+export default Post;
--- src/pages/timeline/timeline.js
+++ src/pages/timeline/timeline.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState, useRef } from "react";
+// import axios from "axios";
+// import { useSelector } from "react-redux";
+
+//locals
+import Post from "../../components/post/post";
 
 const Timeline = () => {
   const ws = useRef(null);
   const [timeline, setTimeline] = useState([]);
-
+  // const user = useSelector((state) => state.serviceReducer);
   // const dispatch = useDispatch();
   useEffect(() => {
     ws.current = new WebSocket("ws://localhost:8000/ws/timeline/");
@@ -22,9 +27,47 @@ const Timeline = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // const axiosConfig = (token) => ({
+  //   headers: {
+  //     Authorization: `Token ${token}`,
+  //   },
+  // });
+
+  // const handleLike = (id) => {
+  //   axios
+  //     .post(
+  //       `https://follow-kenzie.herokuapp.com/api/timeline/post/${id}/like/`,
+  //       axiosConfig(user.user.token)
+  //     )
+  //     .then((r) => {
+  //       console.log(r.data);
+  //     });
+  // };
+
+  // console.log(user.user.token);
+
   return (
-    <div className="App">
-      <p>{JSON.stringify(timeline.timeline)}</p>
+    <div>
+      {timeline.timeline &&
+        timeline.timeline
+          .sort(() => 0.5 - Math.random())
+          .map((item, i) => {
+            return (
+              <>
+                <Post
+                  kei={i}
+                  author={item.author.username}
+                  title={item.title}
+                  posted_on={item.posted_on}
+                  image={item.image}
+                  description={item.description}
+                  // comment={item.comment}
+                  // like={item.like}
+                  // likeAction={() => handleLike(item.id)}
+                />
+              </>
+            );
+          })}
     </div>
   );
 };
